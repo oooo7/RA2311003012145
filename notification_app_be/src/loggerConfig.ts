36e77f922d9a2cbc5baa914aea16e34e3c5ea9e2
Krissh Batra <krissh@Krisshs-MacBook-Air.local>
2026-05-02 13:10:32 +0530

    fix logger import paths for backend

---
+++ notification_app_be/src/loggerConfig.ts
@@ -0,0 +1,12 @@
+const config = {
+  clientID: "4b7b2e01-ba50-4fe6-b4fe-08e122280fb7",
+  clientSecret: "QbhDHaBeKfAmDHht",
+  email: "kb3918@srmist.edu.in",
+  name: "Krissh Batra",
+  rollNo: "RA2311003012145",
+  accessCode: "QkbpxH",
+  logAPI: "http://20.207.122.201/evaluation-service/logs",
+  authAPI: "http://20.207.122.201/evaluation-service/auth",
+};
+
+export default config;
